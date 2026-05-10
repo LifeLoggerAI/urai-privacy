d0b8e97435479d54027265df7f5b6c01c2124120
LifeLoggerAI <LifeLoggerAI@gmail.com>
2026-05-10 17:08:40 -0500

    Remove Python from IDX dev environment

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,16 +1,8 @@
-{ pkgs, ... }:
-let
-  pythonCommand = pkgs.writeShellScriptBin "python" ''
-    exec ${pkgs.python311}/bin/python3 "$@"
-  '';
-in {
+{ pkgs, ... }: {
   channel = "stable-24.05";
 
   packages = [
     pkgs.nodejs_20
-    pkgs.python311
-    pkgs.python311Packages.pip
-    pythonCommand
     pkgs.firebase-tools
     pkgs.git
   ];
@@ -19,13 +11,11 @@ in {
     extensions = [
       "dbaeumer.vscode-eslint"
       "esbenp.prettier-vscode"
-      "ms-python.python"
     ];
 
     workspace = {
       onCreate = {
         install-node-deps = "npm install";
-        install-python-deps = "python -m pip install -r requirements.txt";
       };
     };
   };
