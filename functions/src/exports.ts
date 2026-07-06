b37fc3bfdd228d72b19fa01055b03fd8617f9525
LifeLoggerAI <LifeLoggerAI@gmail.com>
2026-07-06 03:31:12 -0500

    Use deletion orchestration entry points

--- functions/src/exports.ts
+++ functions/src/exports.ts
@@ -1,22 +1,12 @@
 export {
   createExportRequest,
   createDeletionRequest,
-  processDeletionRequest,
-  executeDeletionRequest,
   writeAuditLog,
   recordAdminAction,
   getPrivacyHealthReport
 } from "./index";
 
 export { processExportRequest } from "./export-functions";
-
-export {
-  getExportDownloadUrl,
-  cleanupExpiredExportPackages
-} from "./export-lifecycle-functions";
-
-export {
-  getConsentPurposeRegistry,
-  updateConsent,
-  evaluateConsent
-} from "./consent-functions";
+export { getExportDownloadUrl, cleanupExpiredExportPackages } from "./export-lifecycle-functions";
+export { processDeletionRequest, executeDeletionRequest } from "./deletion-functions";
+export { getConsentPurposeRegistry, updateConsent, evaluateConsent } from "./consent-functions";
