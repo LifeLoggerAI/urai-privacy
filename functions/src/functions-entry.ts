4505ea2d9b5c8d645276b54ae7d9645cef27b8a1
LifeLoggerAI <LifeLoggerAI@gmail.com>
2026-08-04 14:17:19 -0500

    fix(privacy): make deployed entry point authoritative

--- functions/src/functions-entry.ts
+++ functions/src/functions-entry.ts
@@ -1,12 +1,12 @@
 export {
   createExportRequest,
-  getExportDownloadUrl,
   createDeletionRequest,
   writeAuditLog,
   recordAdminAction,
   getPrivacyHealthReport
 } from "./index";
 
+export { getExportDownloadUrl, cleanupExpiredExportPackages } from "./export-lifecycle-functions";
 export { processDeletionRequest, executeDeletionRequest } from "./deletion-mutation-guard";
 export { processExportRequest } from "./export-request";
 export { setCanonicalConsent, evaluateCanonicalConsent } from "./consent-api";
